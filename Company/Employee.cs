aa27774b16b0b78ad679dc954a68c6406729f207
nsutton78 <nsutton78@gmail.com>
2018-06-26 22:34:32 +0100

    added project files

---
+++ Company/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company
+{
+    class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int EmpCode { get; set; }
+        public string Designation { get; set; }
+
+        public string GetEmployeeName()
+        {
+            return FirstName + ' ' + LastName;
+        }
+    }
+}
